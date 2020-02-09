c33065a29129f6e86bbcac6e1c34c0b826376467
Daniel Smith <danielmsmith@gmail.com>
2020-02-09 21:45:52 +1000

    Updates

---
+++ precache-manifest.261bdb28575a6b97fe05fd73c47e0230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f610001f276a4964c1706fbf8bc3ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7791496d7ee73da3989d",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "199f4d140b6cd89e7a02",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7791496d7ee73da3989d",
+    "url": "/static/js/2.0846b22e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.0846b22e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199f4d140b6cd89e7a02",
+    "url": "/static/js/main.f09e7ff7.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
